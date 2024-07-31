3e3c563cebcdb9f81f22be8bd1ed403e341e9ab1
spvce <dujldo.7@gmail.com>
2024-07-31 23:13:20 +0200

    Added external Logger

---
+++ Source/TinyIDS/TinyIDS/Utils/Logger.cs
@@ -0,0 +1,45 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyIDS.Utils
+{
+    public enum Verbosity
+    {
+        None,
+        Basic,
+        Detailed
+    }
+
+    public class Logger
+    {
+        private readonly Verbosity _verbosity;
+
+        public Logger(Verbosity verbosity)
+        {
+            _verbosity = verbosity;
+        }
+
+        public void Log(string message, Verbosity requiredVerbosity)
+        {
+            if (_verbosity >= requiredVerbosity)
+            {
+                AnsiConsole.MarkupLine(message);
+            }
+        }
+
+        public void LogDeviceStatistics(string statistics)
+        {
+            //AnsiConsole.Write(new Panel(statistics)
+            //    .Header("Device Statistics")
+            //    .Border(BoxBorder.Rounded)
+            //    .BorderColor(Color.Grey)
+            //    .Expand());
+
+            Console.WriteLine(statistics);
+        }
+    }
+}
